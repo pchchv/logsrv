f327a8f6b839330fad0b79944b245c90b85ad55a
pchchv <ipchchv@gmail.com>
2022-10-21 02:00:55 +0300

    Implement a Provider func

--- login/provider.go
+++ login/provider.go
@@ -6,6 +6,19 @@ type Provider func(config map[string]string) (Backend, error)
 var provider = map[string]Provider{}
 var providerDescription = map[string]*ProviderDescription{}
 
+// Registers a factory method by the provider name
+func RegisterProvider(desc *ProviderDescription, factoryMethod Provider) {
+	provider[desc.Name] = factoryMethod
+	providerDescription[desc.Name] = desc
+}
+
+// Returns a registered provider by its name
+// The bool return parameter indicated, if there was such a provider
+func GetProvider(providerName string) (Provider, bool) {
+	p, exist := provider[providerName]
+	return p, exist
+}
+
 // GetProviderDescription returns the metainfo for a provider
 func GetProviderDescription(providerName string) (*ProviderDescription, bool) {
 	p, exist := providerDescription[providerName]
