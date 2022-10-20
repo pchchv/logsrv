f7b59f4bf43fe6afe7edb054fe977da4b10abdd8
pchchv <ipchchv@gmail.com>
2022-10-21 01:48:35 +0300

    Implement a Logger

--- logging/logger.go
+++ logging/logger.go
@@ -1,5 +1,31 @@
 package logging
 
-import "github.com/sirupsen/logrus"
+import (
+	"time"
+
+	"github.com/sirupsen/logrus"
+)
 
 var Logger *logrus.Logger
+
+func init() {
+	Set("info", false)
+}
+
+// Set creates a new Logger with the matching specification
+func Set(level string, textLogging bool) error {
+	l, err := logrus.ParseLevel(level)
+	if err != nil {
+		return err
+	}
+
+	logger := logrus.New()
+	if textLogging {
+		logger.Formatter = &logrus.TextFormatter{}
+	} else {
+		logger.Formatter = &LogstashFormatter{TimestampFormat: time.RFC3339Nano}
+	}
+	logger.Level = l
+	Logger = logger
+	return nil
+}
