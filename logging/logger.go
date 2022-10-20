134683d416302e19e6cc7420d6f0a6cbe2299ee9
pchchv <ipchchv@gmail.com>
2022-10-21 01:56:37 +0300

    Implement logger lifecycle

--- logging/logger.go
+++ logging/logger.go
@@ -1,12 +1,20 @@
 package logging
 
 import (
+	"encoding/json"
+	"os"
+	"strings"
 	"time"
 
 	"github.com/sirupsen/logrus"
 )
 
-var Logger *logrus.Logger
+var (
+	Logger *logrus.Logger
+	// The of cookies which should not be logged
+	AccessLogCookiesBlacklist = []string{}
+	LifecycleEnvVars          = []string{"BUILD_NUMBER", "BUILD_HASH", "BUILD_DATE"}
+)
 
 func init() {
 	Set("info", false)
@@ -28,4 +36,57 @@ func Set(level string, textLogging bool) error {
 	logger.Level = l
 	Logger = logger
 	return nil
+}
+
+// Logs the start of an application with the configuration struct or map as paramter
+func LifecycleStart(appName string, args interface{}) {
+	fields := logrus.Fields{}
+	jsonString, err := json.Marshal(args)
+	if err == nil {
+		err := json.Unmarshal(jsonString, &fields)
+		if err != nil {
+			fields["parse_error"] = err.Error()
+		}
+	}
+	fields["type"] = "lifecycle"
+	fields["event"] = "start"
+	for _, env := range LifecycleEnvVars {
+		if os.Getenv(env) != "" {
+			fields[strings.ToLower(env)] = os.Getenv(env)
+		}
+	}
+	Logger.WithFields(fields).Infof("starting application: %v", appName)
+}
+
+// Logs the stop of an application
+func LifecycleStop(appName string, signal os.Signal, err error) {
+	fields := logrus.Fields{
+		"type":  "lifecycle",
+		"event": "stop",
+	}
+	if signal != nil {
+		fields["signal"] = signal.String()
+	}
+	if os.Getenv("BUILD_NUMBER") != "" {
+		fields["build_number"] = os.Getenv("BUILD_NUMBER")
+	}
+	if err != nil {
+		Logger.WithFields(fields).
+			WithError(err).
+			Errorf("stopping application: %v (%v)", appName, err)
+	} else {
+		Logger.WithFields(fields).Infof("stopping application: %v (%v)", appName, signal)
+	}
+}
+
+// Logs the stop of an application
+func ServerClosed(appName string) {
+	fields := logrus.Fields{
+		"type":  "application",
+		"event": "stop",
+	}
+	if os.Getenv("BUILD_NUMBER") != "" {
+		fields["build_number"] = os.Getenv("BUILD_NUMBER")
+	}
+	Logger.WithFields(fields).Infof("http server was closed: %v", appName)
 }
