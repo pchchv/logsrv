56527b0ca50f36c52d05b658e252ea0573fb7926
pchchv <ipchchv@gmail.com>
2022-10-21 14:11:03 +0300

    Implement a OAuth Manager configs funcs

--- oauth2/manager.go
+++ oauth2/manager.go
@@ -1,6 +1,9 @@
 package oauth2
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 // Responsible for handling user requests in the oauth flow
 // Must pick the right configuration and run the oauth redirect
@@ -17,4 +20,49 @@ func NewManager() *Manager {
 		startFlow:    StartFlow,
 		authenticate: Authenticate,
 	}
+}
+
+// AddConfig for a provider
+func (manager *Manager) AddConfig(providerName string, opts map[string]string) error {
+	p, exist := GetProvider(providerName)
+
+	if !exist {
+		return fmt.Errorf("no provider for name %v", providerName)
+	}
+
+	cfg := Config{
+		Provider: p,
+		AuthURL:  p.AuthURL,
+		TokenURL: p.TokenURL,
+	}
+
+	clientID, exist := opts["client_id"]
+	if !exist {
+		return fmt.Errorf("missing parameter client_id")
+	}
+	cfg.ClientID = clientID
+
+	clientSecret, exist := opts["client_secret"]
+	if !exist {
+		return fmt.Errorf("missing parameter client_secret")
+	}
+	cfg.ClientSecret = clientSecret
+
+	if scope, exist := opts["scope"]; exist {
+		cfg.Scope = scope
+	} else {
+		cfg.Scope = p.DefaultScopes
+	}
+
+	if redirectURI, exist := opts["redirect_uri"]; exist {
+		cfg.RedirectURI = redirectURI
+	}
+
+	manager.configs[providerName] = cfg
+	return nil
+}
+
+// GetConfigs of the manager
+func (manager *Manager) GetConfigs() map[string]Config {
+	return manager.configs
 }
