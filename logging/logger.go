89d2767c7eac977389efd8828cb79f1814663423
pchchv <ipchchv@gmail.com>
2022-10-22 13:49:05 +0300

    Implement a logger funcs

--- logging/logger.go
+++ logging/logger.go
@@ -2,6 +2,7 @@ package logging
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"os"
 	"strings"
@@ -37,6 +38,66 @@ func Set(level string, textLogging bool) error {
 	logger.Level = l
 	Logger = logger
 	return nil
+}
+
+// Logs an access entry with call duration and status code
+func Access(r *http.Request, start time.Time, statusCode int) {
+	e := access(r, start, statusCode, nil)
+	var msg string
+	if len(r.URL.RawQuery) == 0 {
+		msg = fmt.Sprintf("%v ->%v %v", statusCode, r.Method, r.URL.Path)
+	} else {
+		msg = fmt.Sprintf("%v ->%v %v?...", statusCode, r.Method, r.URL.Path)
+	}
+	if statusCode >= 200 && statusCode <= 399 {
+		e.Info(msg)
+	} else if statusCode >= 400 && statusCode <= 499 {
+		e.Warn(msg)
+	} else {
+		e.Error(msg)
+	}
+}
+
+// Logs an error while accessing
+func AccessError(r *http.Request, start time.Time, err error) {
+	e := access(r, start, 0, err)
+	e.Errorf("ERROR ->%v %v", r.Method, r.URL.Path)
+}
+
+func access(r *http.Request, start time.Time, statusCode int, err error) *logrus.Entry {
+	url := r.URL.Path
+	if r.URL.RawQuery != "" {
+		url += "?" + r.URL.RawQuery
+	}
+	fields := logrus.Fields{
+		"type":       "access",
+		"@timestamp": start,
+		"remote_ip":  getRemoteIp(r),
+		"host":       r.Host,
+		"url":        url,
+		"method":     r.Method,
+		"proto":      r.Proto,
+		"duration":   time.Since(start).Nanoseconds() / 1000000,
+		"User_Agent": r.Header.Get("User-Agent"),
+	}
+	if statusCode != 0 {
+		fields["response_status"] = statusCode
+	}
+	if err != nil {
+		fields[logrus.ErrorKey] = err.Error()
+	}
+	setCorrelationIds(fields, r.Header)
+	cookies := map[string]string{}
+	for _, c := range r.Cookies() {
+		if !contains(AccessLogCookiesBlacklist, c.Name) {
+			cookies[c.Name] = c.Value
+		}
+	}
+	if len(cookies) > 0 {
+		fields["cookies"] = cookies
+	}
+
+	return Logger.WithFields(fields)
 }
 
 // Return a log entry for application logs, prefilled with the correlation ids out of the supplied request
@@ -101,6 +162,16 @@ func ServerClosed(appName string) {
 	Logger.WithFields(fields).Infof("http server was closed: %v", appName)
 }
 
+func getRemoteIp(r *http.Request) string {
+	if r.Header.Get("X-Cluster-Client-Ip") != "" {
+		return r.Header.Get("X-Cluster-Client-Ip")
+	}
+	if r.Header.Get("X-Real-Ip") != "" {
+		return r.Header.Get("X-Real-Ip")
+	}
+	return strings.Split(r.RemoteAddr, ":")[0]
+}
+
 func setCorrelationIds(fields logrus.Fields, h http.Header) {
 	correlationId := GetCorrelationId(h)
 	if correlationId != "" {
@@ -111,4 +182,13 @@ func setCorrelationIds(fields logrus.Fields, h http.Header) {
 	if userCorrelationId != "" {
 		fields["user_correlation_id"] = userCorrelationId
 	}
+}
+
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
 }
