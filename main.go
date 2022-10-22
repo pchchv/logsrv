aebb25285112d802f332da86ad94242c666d4022
pchchv <ipchchv@gmail.com>
2022-10-22 14:07:32 +0300

    Implement a htpasswd

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"os/signal"
 	"syscall"
 
+	_ "github.com/pchchv/logsrv/htpasswd"
 	"github.com/pchchv/logsrv/logging"
 	"github.com/pchchv/logsrv/login"
 )
@@ -20,28 +21,21 @@ func main() {
 		exit(nil, err)
 	}
 	logging.AccessLogCookiesBlacklist = append(logging.AccessLogCookiesBlacklist, config.CookieName)
-
 	configToLog := *config
 	configToLog.JwtSecret = "..."
 	logging.LifecycleStart(appName, configToLog)
-
 	h, err := login.NewHandler(config)
 	if err != nil {
 		exit(nil, err)
 	}
-
 	handlerChain := logging.NewLogMiddleware(h)
-
 	stop := make(chan os.Signal)
 	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
-
 	port := config.Port
 	if port != "" {
 		port = fmt.Sprintf(":%s", port)
 	}
-
 	httpSrv := &http.Server{Addr: port, Handler: handlerChain}
-
 	go func() {
 		if err := httpSrv.ListenAndServe(); err != nil {
 			if err == http.ErrServerClosed {
@@ -52,9 +46,7 @@ func main() {
 		}
 	}()
 	logging.LifecycleStop(appName, <-stop, nil)
-
 	ctx, ctxCancel := context.WithTimeout(context.Background(), config.GracePeriod)
-
 	httpSrv.Shutdown(ctx)
 	ctxCancel()
 }
